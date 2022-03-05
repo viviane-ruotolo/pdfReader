03b90335859fbb0c40faff7410c98fe3d58e9fbd
Viviane Ruotolo <vivianeruotolo@hotmail.com>
2022-03-05 16:28:12 -0300

    Code organization

--- extractImage.py
+++ extractImage.py
@@ -10,11 +10,9 @@ for current_page in range(len(pdf_document)):
         xref = image[0]
         pix = fitz.Pixmap(pdf_document, xref)
         if pix.n < 5:        # this is GRAY or RGB
-            #pix.writePNG("page%s-%s.png" % (current_page, xref))
             pix.writePNG("Images/page%s-%s.png" % (current_page, xref))
         else:                # CMYK: convert to RGB first
             pix1 = fitz.Pixmap(fitz.csRGB, pix)
-            #pix1.writePNG("page%s-%s.png" % (current_page, xref))
             pix1.writePNG("Images/page%s-%s.png" % (current_page, xref))
             pix1 = None
         pix = None
